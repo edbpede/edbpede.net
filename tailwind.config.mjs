be9a2638b5eb2ad3555aef5d60a37a1302710694
edbpede <144238562+edbpede@users.noreply.github.com>
2025-01-27 22:52:19 +0100

    feat: simple page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,25 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			animation: {
+				'spin-slow': 'spin 20s linear infinite',
+				'pulse-slow': 'pulse 3s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+			},
+			scale: {
+				'85': '0.85',
+				'115': '1.15',
+			},
+			blur: {
+				xs: '2px',
+			},
+			height: {
+				screen: ['100vh', '100dvh'],
+			},
+			minHeight: {
+				screen: ['100vh', '100dvh'],
+			},
+		},
 	},
 	plugins: [],
 }
