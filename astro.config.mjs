955de4a10cf86eff1078a40f30e8f1c436d9c712
edbpede <144238562+edbpede@users.noreply.github.com>
2025-02-12 13:13:26 +0100

    feat: upgrade astro + tailwindcss vite

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
-import tailwind from '@astrojs/tailwind';
-
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
 import icon from 'astro-icon';
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon()]
+  integrations: [icon()],
+  vite: {
+    css: {
+      postcss: {
+        plugins: [
+          tailwindcss,
+          autoprefixer,
+        ],
+      },
+    },
+  }
 });
